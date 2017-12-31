d16b46196fcbbda4d53d57b8d431d1adfcb534e8
Laura Robertson <mslaura.mae@gmail.com>
2017-12-30 17:58:50 -0800

    Search input form added

--- App.js
+++ App.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+/* eslint-disable */
 
-export default class App extends React.Component {
+'use strict';
+
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  NavigatorIOS,
+} from 'react-native';
+import SearchProduct from './SearchProduct';
+
+export default class App extends Component<{}> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Skincare App',
+          component: SearchProduct,
+        }}/>
     );
   }
 }
@@ -16,8 +25,5 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
---
+++ SearchProduct.js
@@ -0,0 +1,125 @@
+/* eslint-disable */
+
+import React, { Component } from 'react';
+
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Button,
+  ActivityIndicator,
+} from 'react-native';
+
+function url(input) {
+  input = input.replace(new RegExp(' ', 'g'), '+');
+  return 'https://skincare-api.herokuapp.com/product?q=' + input;
+}
+
+export default class SearchPage extends Component<{}> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchString: 'Glycolic acid',
+      isLoading: false,
+      message: '',
+      selectedTab: 'welcome'
+    };
+  }
+
+  _onSearchTextChanged = (event) => {
+    this.setState({ searchString: event.nativeEvent.text });
+  };
+
+  _onSearchPressed = () => {
+    const query = url('product_name', this.state.searchString, 1);
+    this._query(query);
+  };
+
+  _query = (query) => {
+    this.setState({ isLoading: true });
+    fetch(query)
+    .then(response => response.json())
+    .then(json => this._response(json))
+    .catch(error =>
+      this.setState({
+        isLoading: false,
+        message: 'An error occured' + error
+      })
+    );
+  };
+
+  _response = (response) => {
+    this.setState({ isLoading: false, message: ''});
+    if (response.length) {
+      this.props.navigator.push({
+        title: 'Results',
+        component: SearchResults,
+        passProps: {products: response}
+      });
+    } else {
+      this.setState({ message: 'No results. Please try again.'});
+    }
+  };
+
+  render() {
+    const spinner = this.state.isLoading ?
+    <ActivityIndicator size='large'/> : null;
+    return (
+        <View style={styles.container}>
+
+          <Text style={styles.description}>
+          SEARCH PRODUCTS
+          </Text>
+
+          <View style={styles.flowRight}>
+
+            <TextInput
+            style={styles.searchInput}
+            value={this.state.searchString}
+            onChange={this._onSearchTextChanged}
+            placeholder='Search'/>
+            <Button
+            onPress={this._onSearchPressed}
+            color='#333333'
+            title='GO'
+            />
+
+          </View>
+          {spinner}
+        </View>
+      );
+  }
+}
+
+const styles = StyleSheet.create({
+  description: {
+    fontSize: 12,
+    fontWeight: '900',
+    letterSpacing: 3,
+    paddingBottom: 5,
+    color: '#333333',
+    marginBottom: 10
+  },
+  container: {
+    padding: 30,
+    marginTop: 65,
+    alignItems: 'center'
+  },
+  flowRight: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    alignSelf: 'stretch',
+  },
+  searchInput: {
+    fontWeight: '200',
+    height: 36,
+    padding: 4,
+    marginRight: 5,
+    flexGrow: 1,
+    fontSize: 18,
+    borderBottomWidth: 1,
+    borderColor: '#333333',
+    color: '#333333',
+  },
+});
